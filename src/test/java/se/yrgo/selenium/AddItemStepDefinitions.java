381011d9f8de8a98d5bf7420d6ad7c342b361b53
Älva <alvlac0326@skola.goteborg.se>
2024-12-02 13:28:39 +0100

    Adding 'real' assignment data

---
+++ src/test/java/se/yrgo/selenium/AddItemStepDefinitions.java
@@ -0,0 +1,5 @@
+package se.yrgo.selenium;
+
+public class AddItemStepDefinitions {
+
+}
--- src/test/java/se/yrgo/selenium/RunCucumberTests.java
+++ src/test/java/se/yrgo/selenium/RunCucumberTests.java
@@ -1,17 +1,14 @@
-package se.yrgo.example;
+package se.yrgo.selenium;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-@Disabled
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("example")
+@SelectClasspathResource("selenium")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTests {
 }
